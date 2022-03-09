c14c7cfa0e357bf042aab0f30860b96fa9e00b8d
Javier fernandez <javierfernandezdeveloper@gmail.com>
2022-03-09 17:11:19 +0100

    boton azul en rma btnAzul

--- app/src/main/java/com/javidev/gitpruebasjavikidoandroid/MainActivity.kt
+++ app/src/main/java/com/javidev/gitpruebasjavikidoandroid/MainActivity.kt
@@ -3,10 +3,15 @@ package com.javidev.gitpruebasjavikidoandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.javidev.gitpruebasjavikidoandroid.ui.theme.GitPruebasJavikidoAndroidTheme
 
@@ -26,10 +31,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column() {
+
+        Text(text = "Hello $name!", modifier = Modifier.background(Color.LightGray))
+
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Boton azul", color = Color.Black)
+        }
+
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     GitPruebasJavikidoAndroidTheme {
