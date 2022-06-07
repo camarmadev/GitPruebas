234c2efa9b23441f1b4151bf2dbfcdcbe796c579
Javier fernandez <javierfernandezdeveloper@gmail.com>
2022-06-07 18:32:32 +0200

    ignore

---
+++ app/src/main/java/com/javidev/gitpruebasjavikidoandroid/PruebasIgnore.kt
@@ -0,0 +1,9 @@
+package com.javidev.gitpruebasjavikidoandroid
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PruebasGitIgnore() {
+    Text(text = "pruebas de gitIgnore")
+}
