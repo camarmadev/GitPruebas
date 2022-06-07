98388f0121ebd79564a50b5ed1e9b32834054673
Javier fernandez <javierfernandezdeveloper@gmail.com>
2022-06-07 19:04:03 +0200

    ignoe

--- app/src/main/java/com/javidev/gitpruebasjavikidoandroid/PruebasIgnore.kt
+++
@@ -1,9 +0,0 @@
-package com.javidev.gitpruebasjavikidoandroid
-
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun PruebasGitIgnore() {
-    Text(text = "pruebas de gitIgnore")
-}
