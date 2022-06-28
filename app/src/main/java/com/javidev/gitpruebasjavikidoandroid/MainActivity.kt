36313e151e0cb71be5d182b876645c990d8e63c6
Javier fernandez <javierfernandezdeveloper@gmail.com>
2022-06-28 18:34:39 +0200

    probando un push a el origin

--- app/src/main/java/com/javidev/gitpruebasjavikidoandroid/MainActivity.kt
+++ app/src/main/java/com/javidev/gitpruebasjavikidoandroid/MainActivity.kt
@@ -56,6 +56,9 @@ fun Greeting(name: String) {
         Button(onClick = { /*TODO*/ }) {
             Text(text = "Branch master", color = Color.Black)
         }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Branch master", color = Color.Black)
+        }
 
     }
 }
