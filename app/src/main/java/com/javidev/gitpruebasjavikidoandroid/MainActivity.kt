b3d0a8c7fd5c2dca398cb44d185b163e06b59609
Javier fernandez <javierfernandezdeveloper@gmail.com>
2022-06-28 18:21:59 +0200

    p

--- app/src/main/java/com/javidev/gitpruebasjavikidoandroid/MainActivity.kt
+++ app/src/main/java/com/javidev/gitpruebasjavikidoandroid/MainActivity.kt
@@ -56,17 +56,6 @@ fun Greeting(name: String) {
         Button(onClick = { /*TODO*/ }) {
             Text(text = "Branch master", color = Color.Black)
         }
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Branch master", color = Color.Black)
-        }
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Branch master", color = Color.Black)
-        }
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Branch master", color = Color.Black)
-        }
-
-
 
     }
 }
