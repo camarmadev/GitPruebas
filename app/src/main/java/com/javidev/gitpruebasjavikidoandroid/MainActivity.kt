ba9129e51cfb394b810b83a27b0017da572f0aa0
Javier fernandez <javierfernandezdeveloper@gmail.com>
2022-08-13 05:00:11 +0200

    lista creado repaso compose

--- app/src/main/java/com/javidev/gitpruebasjavikidoandroid/MainActivity.kt
+++ app/src/main/java/com/javidev/gitpruebasjavikidoandroid/MainActivity.kt
@@ -3,34 +3,10 @@ package com.javidev.gitpruebasjavikidoandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.R
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.javidev.gitpruebasjavikidoandroid.screens.Greeting
-import com.javidev.gitpruebasjavikidoandroid.screens.Home2
+import com.javidev.gitpruebasjavikidoandroid.screens.ListMediaItem
 import com.javidev.gitpruebasjavikidoandroid.ui.theme.GitPruebasJavikidoAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,7 +17,8 @@ class MainActivity : ComponentActivity() {
                 val controller = rememberNavController()
                 Surface(color = MaterialTheme.colors.background) {
                     //Navigation()
-                    Home2(controller = controller)
+                    //Home2(controller = controller)
+                    ListMediaItem()
                 }
             }
         }
